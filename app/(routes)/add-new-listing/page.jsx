912aa34399ff310d6c830dde761d4334dca2ac76
Aryan Raina <aryan.raina.553@gmail.com>
2025-07-22 15:44:39 +0530

    edit listing page

--- app/(routes)/add-new-listing/page.jsx
+++ app/(routes)/add-new-listing/page.jsx
@@ -1,17 +1,32 @@
+"use client"
+
 import GoogleAddressSearch from '@/app/_components/GoogleAddressSearch'
 import { Button } from '@/components/ui/button'
-import React from 'react'
+import React, { useState } from 'react'
 
 
 function AddNewListing() {
+    const [selectedAddress,setSelectedAddress] = useState();
+    const [coordinates,setCoordinates] = useState();
+
+    const nextHandler=()=>{
+        console.log(selectedAddress,coordinates);
+    }
     return (
         <div className='mt-10 md:mx-56 lg:mx-80'>
             <div className='p-10 flex flex-col gap-5 items-center justify-center'>
                 <h2 className='font-bold text-2xl'>Add New Listing</h2>
-                <div className='p-10 px-28 rounded-lg border w-full shadow-md flex flex-col gap-5'>
+                <div className='p-10 rounded-lg border w-full shadow-md flex flex-col gap-5'>
                     <h2 className='text-gray-500'>Enter Address to list.</h2>
-                    <GoogleAddressSearch />
-                    <Button>Next</Button>
+                    <GoogleAddressSearch
+                        selectedAddress={(value)=>setSelectedAddress(value)}
+                        setCoordinates = {(value)=>setCoordinates(value)} 
+                    />
+                    <Button
+                        disabled={!selectedAddress || !coordinates}
+                        onClick={nextHandler}>
+                        Next
+                    </Button>
                 </div>
             </div>
         </div>
--- app/_components/GoogleAddressSearch.jsx
+++ app/_components/GoogleAddressSearch.jsx
@@ -2,9 +2,9 @@
 
 import { MapPin } from 'lucide-react';
 import React from 'react'
-import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
+import GooglePlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-google-places-autocomplete'
 
-function GoogleAddressSearch() {
+function GoogleAddressSearch({selectedAddress,setCoordinates}) {
     return (
         <div className='flex items-center w-full'>
             <MapPin className='h-10 w-10 p-2 rounded-l-lg text-primary bg-orange-200'/>
@@ -15,7 +15,12 @@ function GoogleAddressSearch() {
                     isClearable: true,
                     className: 'w-full',
                     onChange: (place) => {
-                        console.log(place);
+                        selectedAddress(place);
+                        geocodeByAddress(place.label)
+                        .then(result=>getLatLng(result[0]))
+                        .then(({lat,lng})=>{
+                            setCoordinates({lat,lng})
+                        })
                     }
                 }}
 
